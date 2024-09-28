2c9af3bb0d1d8c0f24258bda2b752ba3d5380c75
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2024-09-29 02:18:05 +0900

    style : 정답 길이 상수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,9 +7,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Application {
+    static final int ANSWER_SIZE = 3;
+
     public static List<Integer> generateAnswer(){
         List<Integer> answer = new ArrayList<>();
-        while(answer.size()<3){
+        while(answer.size()<ANSWER_SIZE){
             int random = Randoms.pickNumberInRange(1, 9);
             if(!answer.contains(random)) answer.add(random);
         }
@@ -19,8 +21,8 @@ public class Application {
         List<Integer> input = new ArrayList<>();
         String in = scanner.nextLine();
         // 길이가 3이 아닌경우
-        if(in.length()!=3) throw new IllegalArgumentException();
-        for (int i = 0; i < 3; i++) {
+        if(in.length()!=ANSWER_SIZE) throw new IllegalArgumentException();
+        for (int i = 0; i < ANSWER_SIZE; i++) {
             int tmp = in.charAt(i) - '0';
             // 숫자로만 입력되지 않은 경우 or 입력 중 중복된 숫자가 있을 경우
             if((tmp<1||tmp>9) || input.contains(tmp)) throw new IllegalArgumentException();
@@ -33,9 +35,9 @@ public class Application {
         int strike = 0;
         int ball = 0;
         List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < ANSWER_SIZE; i++) {
             if(answer.get(i).equals(input.get(i))) strike++;
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < ANSWER_SIZE; j++) {
                 if(i==j) continue;
                 if(answer.get(i).equals(input.get(j))) ball++;
             }
