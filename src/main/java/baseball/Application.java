b93effae9f1e868730d17af3e2eab1a49bb41b35
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2024-09-28 23:08:47 +0900

    java-baseball-6-4 feat #4 : 사용자가 잘못된 값을 입력하면 IllegalArgumentException를 발생시키는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,10 +18,14 @@ public class Application {
     public static List<Integer> getUserInput(Scanner scanner){
         List<Integer> input = new ArrayList<>();
         String in = scanner.nextLine();
+        // 길이가 3이 아닌경우
         if(in.length()!=3) throw new IllegalArgumentException();
         for (int i = 0; i < 3; i++) {
             int tmp = in.charAt(i) - '0';
+            // 숫자로만 입력되지 않은 경우
             if(tmp<1 || tmp>9) throw new IllegalArgumentException();
+            // 중복된 숫자가 있을 경우
+            else if(input.contains(tmp)) throw new IllegalArgumentException();
             input.add(tmp);
         }
         return input;
