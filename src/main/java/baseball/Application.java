5221b6566bdf36adf09e14a80149091213576c8e
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2024-09-28 23:22:13 +0900

    style : 코드 구조 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -22,10 +22,8 @@ public class Application {
         if(in.length()!=3) throw new IllegalArgumentException();
         for (int i = 0; i < 3; i++) {
             int tmp = in.charAt(i) - '0';
-            // 숫자로만 입력되지 않은 경우
-            if(tmp<1 || tmp>9) throw new IllegalArgumentException();
-            // 중복된 숫자가 있을 경우
-            else if(input.contains(tmp)) throw new IllegalArgumentException();
+            // 숫자로만 입력되지 않은 경우 or 입력 중 중복된 숫자가 있을 경우
+            if((tmp<1||tmp>9) || input.contains(tmp)) throw new IllegalArgumentException();
             input.add(tmp);
         }
         return input;
