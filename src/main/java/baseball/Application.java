95ed76780d252b89d1ea521a500d67fd0c72f4bd
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2024-09-28 22:34:15 +0900

    java-baseball-6-2 feat #2 : 사용자에게 숫자를 입력받는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,15 +4,27 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
-    public List<Integer> generateAnswer(){
+    public static List<Integer> generateAnswer(){
         List<Integer> answer = new ArrayList<>();
         while(answer.size()<3){
             int random = Randoms.pickNumberInRange(1, 9);
             if(!answer.contains(random)) answer.add(random);
         }
         return answer;
+    }
+    public static List<Integer> getUserInput(Scanner scanner){
+        List<Integer> input = new ArrayList<>();
+        String in = scanner.nextLine();
+        if(in.length()!=3) throw new IllegalArgumentException();
+        for (int i = 0; i < 3; i++) {
+            int tmp = in.charAt(i) - '0';
+            if(tmp<1 || tmp>9) throw new IllegalArgumentException();
+            input.add(tmp);
+        }
+        return input;
     }
     public static void main(String[] args) {
     }
